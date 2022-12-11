edff85bc37e7a263a2295e8f68dd5e5efc5bde24
Morgan Davis <mtd37@hotmail.com>
2022-12-11 09:49:42 -0600

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -eo pipefail # script will exit on error
 
-region=$(aws configure get region)
+if [[ "${AWS_REGION}" == "" ]]; then
+  AWS_REGION=$(aws configure get region) || true
+fi
+
 stack_name=$1
 if [[ "$stack_name" == "" ]]; then
   stack_name="terraform-backend" # default name
 fi
-echo "Deploying terraform backend in region: ${region}"
+echo "Deploying terraform backend in region: ${AWS_REGION}"
 
 # TODO add tags
-aws cloudformation deploy --region ${region} --stack-name ${stack_name} \
-  --template-file file://cloudformation/backend.yml \
+aws cloudformation deploy --region ${AWS_REGION} --stack-name ${stack_name} \
+  --template-file cloudformation/backend.yml \
   --capabilities CAPABILITY_NAMED_IAM
